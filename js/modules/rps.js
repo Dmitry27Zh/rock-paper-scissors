a17a113d07e846ac8e84eea97f3faf2b23fd7653
Dmitry <dmitry27zh@gmail.com>
2022-12-30 15:38:33 +0500

    Add game round logic

--- js/modules/rps.js
+++ js/modules/rps.js
@@ -1,5 +1,10 @@
+import { getRandomItem } from '../utils/array.js'
+
 const initRPS = () => {
   const selectionButtons = document.querySelectorAll('[data-selection]')
+  const resultsContainer = document.querySelector('[data-results')
+  const userScoreElement = document.querySelector('[data-score="user"]')
+  const computerScoreElement = document.querySelector('[data-score="computer"]')
 
   selectionButtons.forEach((selectionButton) => {
     selectionButton.addEventListener('click', () => {
@@ -8,6 +13,13 @@ const initRPS = () => {
       round(selection)
     })
   })
+
+  const round = (userSelection) => {
+    const computerSelection = getRandomItem(SELECTIONS)
+    const isUserWinner = userSelection.beats === computerSelection.name
+    const isComputerWinner = computerSelection.beats === userSelection.name
+    updateScores([userScoreElement, computerScoreElement], [isUserWinner, isComputerWinner])
+  }
 }
 
 const SELECTIONS = [
@@ -28,8 +40,23 @@ const SELECTIONS = [
   },
 ]
 
-const round = (userSelection) => {
-  console.log(userSelection)
+const createRecentTemplate = (userSelection, computerSelection, isUserWinner = false, isComputerWinner = false) => {
+  const userWinnerClassString = isUserWinner ? 'winner' : ''
+  const computerWinnerClassString = isUserWinner ? 'winner' : ''
+
+  return `
+    <li class="rps__recent rps__results-columns">
+      <div class="rps__results-selection ${userWinnerClassString}">${userSelection.value}</div>
+      <div class="rps__results-selection ${computerWinnerClassString}">${computerSelection.value}</div>
+    </li>
+  `
+}
+
+const updateScores = (elementsList, isWinnerList) => {
+  elementsList.forEach((element, index) => {
+    const currentScore = Number(element.textContent)
+    element.textContent = currentScore + isWinnerList[index]
+  })
 }
 
 initRPS()
